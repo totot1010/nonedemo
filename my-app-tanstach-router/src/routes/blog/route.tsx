0ba29827550ab5018389ad5d03b906f89ee45f63
yutocreate <1otuy26@gmail.com>
2025-09-07 12:56:00 +0900

    react-tutorial: BlogおよびProductsルートの新規作成。各ルートにレイアウトとネストされたコンポーネントを追加し、スタイルを整備。

--- my-app-tanstach-router/src/routes/blog/route.tsx
+++ my-app-tanstach-router/src/routes/blog/route.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_blog")({
+export const Route = createFileRoute("/blog")({
   component: BlogLayout,
 });
 
